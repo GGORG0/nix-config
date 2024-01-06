103fcf112d910bdf8166991fe578826598cecc10
GGORG <GGORG0@protonmail.com>
2024-01-06 15:47:00 +0100

    sddm theme fixed??

--- modules/nixos/sddm.nix
+++ modules/nixos/sddm.nix
@@ -4,7 +4,13 @@
   pkgs,
   ...
 }: {
-  environment.systemPackages = [pkgs.catppuccin-sddm-corners];
+  environment.systemPackages = with pkgs; [
+    catppuccin-sddm-corners
+    libsForQt5.qt5.qtsvg
+    libsForQt5.qt5.qtgraphicaleffects
+    libsForQt5.qt5.qtquickcontrols2
+  ];
+
   services.xserver.displayManager.sddm = {
     enable = true;
     theme = "catppuccin-sddm-corners";
