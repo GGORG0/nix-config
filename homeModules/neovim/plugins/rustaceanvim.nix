88bf5946b7eb1aa6bcd10568fafc12a51881964c
GGORG <GGORG0@protonmail.com>
2024-10-03 12:05:04 +0200

    make rust inlay hints less annoying

--- homeModules/neovim/plugins/rustaceanvim.nix
+++ homeModules/neovim/plugins/rustaceanvim.nix
@@ -30,17 +30,6 @@
           "target"
         ];
 
-        inlayHints = {
-          bindingModeHints.enable = true;
-          closureStyle = "rust_analyzer";
-          closureReturnTypeHints.enable = "always";
-          discriminantHints.enable = "always";
-          expressionAdjustmentHints.enable = "always";
-          implicitDrops.enable = true;
-          lifetimeElisionHints.enable = "always";
-          rangeExclusiveHints.enable = true;
-        };
-
         procMacro.enable = true;
 
         rustc.source = "discover";
