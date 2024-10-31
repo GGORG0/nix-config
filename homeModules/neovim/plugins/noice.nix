6eafad5ea4c6ca6bb532f898bdd2ab770322d06a
GGORG <GGORG0@protonmail.com>
2024-10-31 19:55:35 +0100

    chore: update flake.lock

--- homeModules/neovim/plugins/noice.nix
+++ homeModules/neovim/plugins/noice.nix
@@ -3,18 +3,20 @@ _: {
   programs.nixvim.plugins.noice = {
     enable = true;
 
-    # override markdown rendering so that cmp and other plugins use Treesitter
-    lsp.override = {
-      "cmp.entry.get_documentation" = true;
-      "vim.lsp.util.convert_input_to_markdown_lines" = true;
-      "vim.lsp.util.stylize_markdown" = true;
-    };
+    settings = {
+      # override markdown rendering so that cmp and other plugins use Treesitter
+      lsp.override = {
+        "cmp.entry.get_documentation" = true;
+        "vim.lsp.util.convert_input_to_markdown_lines" = true;
+        "vim.lsp.util.stylize_markdown" = true;
+      };
 
-    presets = {
-      bottom_search = true;
-      command_palette = true;
-      long_message_to_split = true;
-      inc_rename = true;
+      presets = {
+        bottom_search = true;
+        command_palette = true;
+        long_message_to_split = true;
+        inc_rename = true;
+      };
     };
   };
 }
