7a241bbf452afe7a76c3802706f45cd28f35e9cf
GGORG <GGORG0@protonmail.com>
2024-11-02 10:52:13 +0100

    neovim/cmp: add the ability to cycle through completions

--- homeModules/neovim/plugins/cmp.nix
+++ homeModules/neovim/plugins/cmp.nix
@@ -36,7 +36,43 @@ _: {
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
           "<C-Space>" = "cmp.mapping.complete()";
           "<C-e>" = "cmp.mapping.abort()";
-          "<CR>" = "cmp.mapping.confirm({ select = true })"; # TODO: make this a bit less annoying
+          "<CR>" = ''
+            cmp.mapping(function(fallback)
+              if cmp.visible() then
+                if require("luasnip").expandable() then
+                  require("luasnip").expand()
+                else
+                  cmp.confirm({
+                    select = true,
+                  })
+                end
+              else
+                fallback()
+              end
+            end)
+          '';
+          "<Tab>" = ''
+            cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif require("luasnip").locally_jumpable(1) then
+                require("luasnip").jump(1)
+              else
+                fallback()
+              end
+            end, { "i", "s" })
+          '';
+          "<S-Tab>" = ''
+            cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_prev_item()
+              elseif require("luasnip").locally_jumpable(-1) then
+                require("luasnip").jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" })
+          '';
         };
 
         experimental.ghost_text = true;
