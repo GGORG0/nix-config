4dac0f2a9a5650cc76f3d1e4a8fb2a43fe8cd1e5
GGORG <GGORG0@protonmail.com>
2024-11-16 13:00:26 +0100

    neovim/lsp: add slint

--- homeModules/neovim/plugins/lsp.nix
+++ homeModules/neovim/plugins/lsp.nix
@@ -1,96 +1,108 @@
 _: {
   # Language server - completion, error checking, etc.
-  programs.nixvim.plugins.lsp = {
-    enable = true;
+  programs.nixvim = {
+    plugins.lsp = {
+      enable = true;
 
-    inlayHints = true;
+      inlayHints = true;
 
-    servers = {
-      clangd.enable = true;
+      servers = {
+        clangd.enable = true;
 
-      rust_analyzer = {
-        enable = true;
-        installCargo = false;
-        installRustc = false;
-        settings = {
-          diagnostics = {
-            enable = true;
-            styleLints.enable = true;
-          };
-
-          checkOnSave = true;
-
-          # check.command = "clippy";
-
-          files.excludeDirs = [
-            ".direnv"
-            ".git"
-            "node_modules"
-          ];
-
-          procMacro.enable = true;
-
-          # rustc.source = "discover";
-
-          inlayHints = {
-            enable = true;
-            showParameterNames = true;
-          };
-        };
-      };
-
-      nil_ls = {
-        enable = true;
-        settings.formatting.command = [ "nixpkgs-fmt" ];
-      };
-
-      ts_ls = {
-        enable = false;
-        filetypes = [
-          "javascript"
-          "javascriptreact"
-          "typescript"
-          "typescriptreact"
-        ];
-        extraOptions = {
+        rust_analyzer = {
+          enable = true;
+          installCargo = false;
+          installRustc = false;
           settings = {
-            javascript = {
-              inlayHints = {
-                includeInlayEnumMemberValueHints = true;
-                includeInlayFunctionLikeReturnTypeHints = true;
-                includeInlayFunctionParameterTypeHints = true;
-                includeInlayParameterNameHints = "all";
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true;
-                includeInlayPropertyDeclarationTypeHints = true;
-                includeInlayVariableTypeHints = true;
-              };
+            diagnostics = {
+              enable = true;
+              styleLints.enable = true;
             };
-            typescript = {
-              inlayHints = {
-                includeInlayEnumMemberValueHints = true;
-                includeInlayFunctionLikeReturnTypeHints = true;
-                includeInlayFunctionParameterTypeHints = true;
-                includeInlayParameterNameHints = "all";
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true;
-                includeInlayPropertyDeclarationTypeHints = true;
-                includeInlayVariableTypeHints = true;
+
+            checkOnSave = true;
+
+            # check.command = "clippy";
+
+            files.excludeDirs = [
+              ".direnv"
+              ".git"
+              "node_modules"
+            ];
+
+            procMacro.enable = true;
+
+            # rustc.source = "discover";
+
+            inlayHints = {
+              enable = true;
+              showParameterNames = true;
+            };
+          };
+        };
+
+        nil_ls = {
+          enable = true;
+          settings.formatting.command = [ "nixpkgs-fmt" ];
+        };
+
+        ts_ls = {
+          enable = false;
+          filetypes = [
+            "javascript"
+            "javascriptreact"
+            "typescript"
+            "typescriptreact"
+          ];
+          extraOptions = {
+            settings = {
+              javascript = {
+                inlayHints = {
+                  includeInlayEnumMemberValueHints = true;
+                  includeInlayFunctionLikeReturnTypeHints = true;
+                  includeInlayFunctionParameterTypeHints = true;
+                  includeInlayParameterNameHints = "all";
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = true;
+                  includeInlayPropertyDeclarationTypeHints = true;
+                  includeInlayVariableTypeHints = true;
+                };
+              };
+              typescript = {
+                inlayHints = {
+                  includeInlayEnumMemberValueHints = true;
+                  includeInlayFunctionLikeReturnTypeHints = true;
+                  includeInlayFunctionParameterTypeHints = true;
+                  includeInlayParameterNameHints = "all";
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = true;
+                  includeInlayPropertyDeclarationTypeHints = true;
+                  includeInlayVariableTypeHints = true;
+                };
               };
             };
           };
         };
+
+        ruff_lsp.enable = true;
+
+        jsonls.enable = true;
+        html.enable = true;
+        cssls.enable = true;
+        eslint.enable = true;
+
+        dockerls.enable = true;
+        docker_compose_language_service.enable = true;
+
+        bashls.enable = true;
+
+        slint_lsp.enable = true;
       };
-
-      ruff_lsp.enable = true;
-
-      jsonls.enable = true;
-      html.enable = true;
-      cssls.enable = true;
-      eslint.enable = true;
-
-      dockerls.enable = true;
-      docker_compose_language_service.enable = true;
-
-      bashls.enable = true;
     };
+    autoCmd = [{
+      event = [
+        "BufRead"
+        "BufNewFile"
+      ];
+      pattern = [ "*.slint" ];
+      command = "set filetype=slint";
+    }];
   };
 }
