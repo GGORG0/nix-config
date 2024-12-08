8097a5706815ba8bf2a83cfd31c4900c9c5f7390
GGORG <GGORG0@protonmail.com>
2024-12-08 12:44:46 +0100

    waybar: deleted persistent workspaces they didnt work well for my multi monitor setup...

--- homeModules/hyprland/waybar.nix
+++ homeModules/hyprland/waybar.nix
@@ -38,7 +38,6 @@
             sort-by-name = true;
             format = "{id}";
             show-special = true;
-            persistent-workspaces = { "*" = 10; };
             on-scroll-up = "${lib.getExe' pkgs.hyprland "hyprctl"} dispatch workspace e+1";
             on-scroll-down = "${lib.getExe' pkgs.hyprland "hyprctl"} dispatch workspace e-1";
           };
