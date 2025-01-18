e8596d35de4a47b6821d03463a3b8c3b15f1e295
GGORG <GGORG0@protonmail.com>
2025-01-18 18:46:05 +0100

    docker: switched to podman and added distrobox

--- nixosModules/docker.nix
+++ nixosModules/docker.nix
@@ -1,16 +1,26 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: {
   options = {
     ggorg.docker = {
-      enable = lib.mkEnableOption "Docker";
+      enable = lib.mkEnableOption "Podman w/Docker compatibility";
     };
   };
 
   config = lib.mkIf config.ggorg.docker.enable {
-    virtualisation.docker.enable = true;
-    ggorg.user.extraGroups = ["docker"];
+    virtualisation.containers.enable = true;
+
+    virtualisation.podman = {
+      enable = true;
+      dockerCompat = true;
+      defaultNetwork.settings.dns_enabled = true;
+    };
+
+    ggorg.user.extraGroups = ["podman"];
+
+    environment.systemPackages = [pkgs.distrobox];
   };
 }
