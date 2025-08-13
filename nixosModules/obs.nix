0b3ef52a20d5fe35b5c49f2d0c8b5b6b99d8eee8
GGORG <GGORG0@protonmail.com>
2025-08-13 19:39:50 +0200

    obs: added plugins

--- nixosModules/obs.nix
+++ nixosModules/obs.nix
@@ -11,15 +11,14 @@
   };
 
   config = lib.mkIf config.ggorg.obs.enable {
-    environment.systemPackages = [pkgs.obs-studio];
-
-    boot.extraModulePackages = with config.boot.kernelPackages; [
-      v4l2loopback
-    ];
-    boot.kernelModules = ["v4l2loopback"];
-    boot.extraModprobeConfig = ''
-      options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
-    '';
-    security.polkit.enable = true;
+    programs.obs-studio = {
+      enable = true;
+      enableVirtualCamera = true;
+      plugins = with pkgs.obs-studio-plugins; [
+        obs-vaapi
+        obs-pipewire-audio-capture
+        input-overlay
+      ];
+    };
   };
 }
